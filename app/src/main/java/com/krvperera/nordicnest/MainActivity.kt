ef619956c75c61a4dc13fa2e044d151df73e7f0f
Rukshan Perera <2173530+KRVPerera@users.noreply.github.com>
2024-01-16 09:57:13 +0200

    add app icons

--- app/src/main/java/com/krvperera/nordicnest/MainActivity.kt
+++ app/src/main/java/com/krvperera/nordicnest/MainActivity.kt
@@ -3,13 +3,28 @@ package com.krvperera.nordicnest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+
 import com.krvperera.nordicnest.ui.theme.NordicNestTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +34,8 @@ class MainActivity : ComponentActivity() {
             NordicNestTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("World")
+                    //Greeting("World")
+                    MessageCard(msg = Message("Android", "Jetpack Compose"))
                 }
             }
         }
@@ -30,16 +46,45 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Text(text = msg.author)
-    Text(text = msg.body)
+    Row (modifier = Modifier.padding(all = 8.dp)) {
+        Image (
+            painter = painterResource(R.drawable.me),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = msg.body,
+
+            )
+        }
+    }
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(
-        msg = Message("Lexi", "Hey, take a look at JectPack")
-    )
+    NordicNestTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+            MessageCard(
+                msg = Message("Lexi", "Hey, take a look at Jet Pack")
+            )
+        }
+    }
 }
 
 
