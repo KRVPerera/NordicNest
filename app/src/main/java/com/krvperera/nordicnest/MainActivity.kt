00830e2fcbd2fc952a7c5146f706c6b2e7406b63
Rukshan Perera <2173530+KRVPerera@users.noreply.github.com>
2024-01-21 01:39:10 +0200

    cleanup unwanted preview

--- app/src/main/java/com/krvperera/nordicnest/MainActivity.kt
+++ app/src/main/java/com/krvperera/nordicnest/MainActivity.kt
@@ -94,11 +94,3 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             modifier = modifier
     )
 }
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NordicNestTheme {
-        Greeting("Android")
-    }
-}
