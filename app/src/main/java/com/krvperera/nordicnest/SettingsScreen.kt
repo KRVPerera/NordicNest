8a944bbccc892c8e03c3edebc8f487dd5b3774ee
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2024-01-28 11:55:33 +0200

    feat: add a image to settings screen

--- app/src/main/java/com/krvperera/nordicnest/SettingsScreen.kt
+++ app/src/main/java/com/krvperera/nordicnest/SettingsScreen.kt
@@ -1,12 +1,21 @@
 package com.krvperera.nordicnest
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -18,6 +27,16 @@ import com.krvperera.nordicnest.ui.theme.NordicNestTheme
 fun SettingsScreen(navController: NavController) {
     Column (modifier = Modifier.padding(all = 8.dp)) {
         Title("Settings")
+        Spacer(modifier = Modifier.height(8.dp))
+        Image(
+            painter = painterResource(R.drawable.me),
+            contentDescription = null,
+            modifier = Modifier
+                .size(300.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+                .align(Alignment.CenterHorizontally)
+        )
         Spacer(modifier = Modifier.fillMaxSize(0.9f))
         NavButtons(navController, "SETTINGS")
     }
