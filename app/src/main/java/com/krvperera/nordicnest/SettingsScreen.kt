45668438050edf051eda239da8865ac0194d1cb3
Rukshan Perera <2173530+KRVPerera@users.noreply.github.com>
2024-02-12 18:29:11 +0200

    feat: image picker working

--- app/src/main/java/com/krvperera/nordicnest/SettingsScreen.kt
+++ app/src/main/java/com/krvperera/nordicnest/SettingsScreen.kt
@@ -1,8 +1,11 @@
 package com.krvperera.nordicnest
 
 import android.Manifest
-import android.content.pm.PackageManager
+import android.content.ContentResolver
+import android.content.res.Resources
+import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
@@ -16,48 +19,63 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
-import com.krvperera.nordicnest.ui.theme.NordicNestTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
-import androidx.core.content.ContextCompat
-import coil.compose.AsyncImage
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import coil.compose.SubcomposeAsyncImage
-import coil.compose.rememberAsyncImagePainter
-import coil.request.ImageRequest
 
+
+@Composable
+fun rememberResourceUri(resourceId: Int): Uri {
+    val context = LocalContext.current
+
+    return remember(resourceId) {
+        with(context.resources) {
+            Uri.Builder()
+                .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+                .authority(getResourcePackageName(resourceId))
+                .appendPath(getResourceTypeName(resourceId))
+                .appendPath(getResourceEntryName(resourceId))
+                .build()
+        }
+    }
+}
 
 @Composable
 fun SettingsScreen(navController: NavController) {
     var isInternetPermissionGiven by remember { mutableStateOf(false) }
     var fabHeight by remember { mutableStateOf(0.dp) }
-    val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestPermission(), onResult = { isGranted ->
+    var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
+
+    val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestPermission(),
+        onResult = { isGranted ->
         if (isGranted) {
             isInternetPermissionGiven = true
         }
     })
+
+    val imagePicker = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia(),
+        onResult = { uri ->
+            selectedImageUri = uri
+    })
+
     if (!isInternetPermissionGiven) {
         SideEffect {
             launcher.launch(Manifest.permission.INTERNET)
@@ -67,29 +85,36 @@ fun SettingsScreen(navController: NavController) {
         Title("Settings")
         Spacer(modifier = Modifier.height(8.dp))
         Column (modifier = Modifier.align(Alignment.CenterHorizontally)) {
-            Image(
-                painter = painterResource(R.drawable.me),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(300.dp)
-                    .clip(CircleShape)
-                    .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
-                    .align(Alignment.CenterHorizontally)
-            )
             if (isInternetPermissionGiven) {
                 SubcomposeAsyncImage(
-                    model = "https://images.unsplash.com/photo-1468853692559-fc594e932a2d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    model = selectedImageUri,
+//                    model = "https://images.unsplash.com/photo-1468853692559-fc594e932a2d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                     loading = {
                         CircularProgressIndicator()
                     },
-                    contentDescription = "Asd"
+                    contentScale = ContentScale.FillWidth,
+                    contentDescription = "Asd",
+                    modifier = Modifier
+                        .size(300.dp)
+                        .clip(CircleShape)
+                        .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+                        .align(Alignment.CenterHorizontally)
+                )
+            } else {
+                Image(
+                    painter = painterResource(R.drawable.me),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(300.dp)
+                        .clip(CircleShape)
+                        .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+                        .align(Alignment.CenterHorizontally)
                 )
             }
             FloatingActionButton(
                 shape = CircleShape,
                 onClick = {
-
-
+                    imagePicker.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
                 },
                 modifier = Modifier
                     .onGloballyPositioned { coordinates ->
@@ -109,3 +134,5 @@ fun SettingsScreen(navController: NavController) {
 //        return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
 //    }
 }
+
+
